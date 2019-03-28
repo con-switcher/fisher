6bea7d2207c0b3bb0a02331329edb104dea98362
taiyong <taiyong@youzan.com>
2019-03-28 20:41:25 +0800

    init

--- src/lwip.cpp
+++ src/lwip.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 
 #include "lwip/opt.h"
+#include "lwip/debug.h"
+#include "lwip/err.h"
+#include "lwip/init.h"
+#include "lwip/ip_addr.h"
+#include "lwip/dns.h"
+#include "lwip/netif.h"
+#include "lwip/stats.h"
+#include "lwip/sys.h"
 
 int main(int argc, char **argv)
 {
