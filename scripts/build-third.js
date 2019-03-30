fc0347cfecb5d4e18af0ee763a024eaeb2f2d8d4
taiyong <taiyong@youzan.com>
2019-03-30 22:17:47 +0800

    1.4.0

--- scripts/build-third.js
+++ scripts/build-third.js
@@ -19,11 +19,15 @@ async function buildLwip() {
 }
 
 async function copyLwipHeader() {
-    let lwipHeader = path.resolve(__dirname, '../lwip/lwip-2.1.2/src/include');
-    let contribHeader = path.resolve(__dirname, '../lwip/contrib-2.1.0/ports/unix/port/include');
+    let lwipHeader = path.resolve(__dirname, '../lwip/lwip/src/include');
+    let lwipIp4Header = path.resolve(__dirname, '../lwip/lwip/src/include/ipv4');
+    let lwipIp6Header = path.resolve(__dirname, '../lwip/lwip/src/include/ipv6');
+    let contribHeader = path.resolve(__dirname, '../lwip/contrib/ports/unix/include');
     let customHeader = path.resolve(__dirname, '../lwip/custom');
     let distDir = path.resolve(__dirname, '../out/lwip/include');
     await fs.copy(lwipHeader, distDir, {overwrite: false, errorOnExist: true});
+    await fs.copy(lwipIp4Header, distDir, {overwrite: false, errorOnExist: true});
+    await fs.copy(lwipIp6Header, distDir, {overwrite: false, errorOnExist: true});
     await fs.copy(contribHeader, distDir, {overwrite: false, errorOnExist: true});
     await fs.copy(customHeader, distDir, {overwrite: false, errorOnExist: true});
 }
