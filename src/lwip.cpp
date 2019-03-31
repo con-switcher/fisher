50236c1137adfb5b23596bd5bb4e6abb41be7624
taiyong <taiyong@youzan.com>
2019-03-31 15:08:18 +0800

    test

--- src/lwip.cpp
+++ src/lwip.cpp
@@ -2,26 +2,14 @@
 #include <iostream>
 #include <cstring>
 
-#include "lwip/opt.h"
-#include "lwip/debug.h"
-#include "lwip/err.h"
-#include "lwip/init.h"
-#include "lwip/ip_addr.h"
-#include "lwip/dns.h"
-#include "lwip/netif.h"
-#include "lwip/stats.h"
-#include "lwip/sys.h"
+
+#include "lwip/ip.h"
+
 
 int main(int argc, char **argv)
 {
-    ip_addr_t ip, netmask, gw;
-    netif interface;
 
-    memset(&interface, 0, sizeof(netif));
-
-    lwip_init();
-
-    std::cout<< "hihi" << std::endl;
+    std::cout<< "hihi" << a << std::endl;
 
     return (0);
 }
